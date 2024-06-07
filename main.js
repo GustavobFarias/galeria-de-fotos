f8a58d6168eeb1d3982f78226b7b919b1ad6dd5f
Gustavo <Gustavofarias@gmail.com>
2024-06-07 15:29:51 -0300

    commit initial

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {  
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('form').on('submit', function(e) {
+        console.log('submit');
+        e.preventDefault();
+    })
+})
